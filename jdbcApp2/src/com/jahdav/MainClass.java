64346b74e2b09e1ef4152d24cca9286a2747813a
pravin jadhav <jadhavpravin7112000@gmail.com>
2024-02-11 19:09:43 +0530

    Updating data into database.

---
+++ jdbcApp2/src/com/jahdav/MainClass.java
@@ -0,0 +1,43 @@
+package com.jahdav;
+// import package
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class MainClass {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		// Load and Register Driver
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		// Establish connection
+		String url = "jdbc:mysql://localhost:3306/jdbc";
+		String user = "root";
+		String password = "Pravin@123";
+		Connection connection = DriverManager.getConnection(url, user, password);
+		
+		
+		// creating statement
+		Statement statement = connection.createStatement();
+		
+		
+		//execute query
+		String sql = "UPDATE STUDENTS SET age=22,city='mumbai andheri' where id=5";
+		int rowAffected = statement.executeUpdate(sql);
+		
+		
+		//process result
+		if( rowAffected != 0 )
+			System.out.println("Data inserted successfully.");
+		else
+			System.out.println("Something went wrong.");
+		
+		
+		//close resource
+		statement.close();
+		connection.close();
+		System.out.println("okk");
+	}
+
+}
